faa14898feae235dfcf34d335d30b6ac6190ee87
Anoop <Anooppopziyan@gmail.com>
2026-05-11 17:28:29 +0530

    dark light theme toggle

--- script.js
+++ script.js
@@ -3,3 +3,15 @@ function scrollToSection(id) {
     behavior: "smooth"
   });
 }
+
+function toggleTheme() {
+  document.body.classList.toggle("dark");
+
+  const btn = document.querySelector(".theme-btn");
+
+  if (document.body.classList.contains("dark")) {
+    btn.textContent = "☀️";
+  } else {
+    btn.textContent = "🌙";
+  }
+}
